2b2236a3fd3c2cfe1d6773be4c13b24402a04c56
M.Arif <muhammadarifmanzoor15@gmail.com>
2026-02-11 23:12:40 +0500

    basic commands infrastructure developed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <algorithm>
+#include <cctype>
+
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Welcome To Employee Management System!" << endl;
+    string command;
+
+    // Commands: add, remove, list, exit
+    string add_command[] = {"ADD", "NEW", "ADD EMPLOYEE", "INSERT EMPLOYEE", "CREATE EMPLOYEE", "REGISTER EMPLOYEE", "REGISTER"};
+    string remove_command[] = {"REMOVE EMPLOYEE", "DELETE EMPLOYEE", "ERASE EMPLOYEE", "FIRE EMPLOYEE", "DISMISS EMPLOYEE", "TERMINATE EMPLOYEE", "REMOVE" , "DELETE", "ERASE", "FIRE", "DISMISS", "TERMINATE"};
+    string list_command[] = {"LIST", "GET", "SHOW EMPLOYEES" , "DISPLAY EMPLOYEES", "VIEW EMPLOYEES" , "LIST EMPLOYEES" , "GET EMPLOYEES", "SHOW", "DISPLAY", "VIEW"};
+    string exit_command[] = {"EXIT" , "QUIT" , "LEAVE" , "CLOSE" , "END" , "STOP"};
+
+     
+
+    
+
+    while (true) {
+        cout << "=> ";
+        cin >> command;
+        transform(command.begin(), command.end(), command.begin(), ::toupper);
+        if (find(add_command, add_command + 7, command) != add_command + 7) {
+            // Code to add an employee
+            cout << "Adding an employee..." << endl;
+        } else if (find(remove_command, remove_command + 12, command) != remove_command + 12) {
+            // Code to remove an employee
+            cout << "Removing an employee..." << endl;
+        } else if (find(list_command, list_command + 10, command) != list_command + 10) {
+            // Code to list all employees
+            cout << "Listing all employees..." << endl;
+        } else if (find(exit_command, exit_command + 6, command) != exit_command + 6) {
+            cout << "Exiting the system. Goodbye!" << endl;
+            break;
+        } else {
+            cout << "Invalid command. Please try again." << endl;
+        }
+    }
     return 0;
 }
